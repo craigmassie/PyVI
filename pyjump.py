8ea7998ccf4d7930c84f412740eab091707b6877
Craig Massie <craigsmassie@gmail.com>
2018-07-23 16:35:26 +0100

    K-ary tree implemented. Tkinter buttons need fixed.

--- pyjump.py
+++ pyjump.py
@@ -1,26 +1,44 @@
 import tkinter as tk
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+import os
+from anytree import Node, RenderTree
 
 window = tk.Tk()
 window.withdraw()
 
 file_path = askopenfilename()
 
-funcs = {}
-
 def open_file(file_path):
     code = (open(file_path, "r"))
 
     lines = code.readlines()
+    # for every line in the code to be read
     for line in lines:
-        if "def" in line:
-            name = line.split()[1]
-            funcs[name] = []
-    
-    print(funcs)
+        #if we define a function, and it's not indented
+        if "def " in line:
+            if not line[0].isspace():
+                name = line.split()[1]
+                last = Node(name, parent=root)
+        #if we have a for loop, checks indent
+        elif "for " in line or "if " in line or "while " in line or "try" in line or "else" in line or "except" in line or "finally" in line:
+            if not line[0].isspace():
+                last = Node(line.strip("\n"), parent =root)
+            else:
+                indented = Node(line.strip("\n"), parent =last)
+        else:
+            try:
+                if not last.name == "Code Block" and not line[0].isspace():
+                    last = Node("Code Block", parent=root)
+            except NameError:
+                continue
+            
 
+root = Node(os.path.basename(file_path))
 open_file(file_path)
+for pre, fill, node in RenderTree(root):
+    print("%s%s" % (pre, node.name))
+
 
 screen_width = int((window.winfo_screenwidth()/2) - 350)
 screen_height = int((window.winfo_screenheight()/2) -212)
@@ -29,12 +47,21 @@ window.geometry("700x425+" + str(screen_width) + "+" + str(screen_height))
 window.deiconify()
 window.title("PyJump")
 
-T = tk.Text(window)
-T.config(font=("Arial"))
-T.pack()
-for key in funcs:
-    T.insert(tk.END, key)
-    T.insert(tk.END, "\n")
-    
+# T = tk.Text(window)
+# T.config(font=("Arial"))
+# T.pack()
 
+def callback():
+    print("Hi")
+
+def update_items(selected):
+    display_nodes(selected)
+
+def display_nodes(node):
+    for child in node.children:
+        b = Button(window, text=child)
+        # b = Button(window, text=child, command = update_items(child))
+        b.pack()
+
+display_nodes(root)
 window.mainloop()
