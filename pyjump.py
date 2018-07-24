3d78d6ea43cd3891e2fa13f8ee8b4f050a688f6c
Craig Massie <craigsmassie@gmail.com>
2018-07-24 22:27:15 +0100

    Tree parent fixes. Comments parsed now ignored.

--- pyjump.py
+++ pyjump.py
@@ -16,17 +16,31 @@ def open_file(file_path):
     lines = code.readlines()
     # for every line in the code to be read
     for line in lines:
+        if line.strip().startswith("#"):
+            continue
         #if we define a function, and it's not indented
-        if "def " in line:
+        if "def " in line or "class " in line:
             if not line[0].isspace():
-                name = line.split()[1]
+                print(line.split())
+                name = line
                 last = Node(name, parent=root)
         #if we have a for loop, checks indent
         elif "for " in line or "if " in line or "while " in line or "try" in line or "else" in line or "except" in line or "finally" in line:
             if not line[0].isspace():
                 last = Node(line.strip("\n"), parent =root)
+            #If the current line is more indented than the last
             else:
-                indented = Node(line.strip("\n"), parent =last)
+                cond = (len(line) - len(line.strip()) - ((len(last.name) + 1) - len(last.name.strip())))
+                if (cond > 0):
+                    indented = Node(line.strip("\n"), parent =last)
+                    last = indented
+                #Need to check if equal or before
+                elif (cond == 0):
+                    indented = Node(line.strip("\n"), parent =last.parent)
+                    last = indented
+                elif (cond < 0):
+                    indented = Node(line.strip("\n"), parent =last.parent.parent)
+                    last = indented
         else:
             try:
                 if not last.name == "Code Block" and not line[0].isspace():
@@ -57,7 +71,7 @@ class Example(QWidget):
         # btn.resize(btn.sizeHint())
         # btn.move(50, 50)       
         self.display_nodes(root)
-        self.setWindowTitle('Tooltips') 
+        self.setWindowTitle('PyVi') 
         self.center()   
         self.show()
 
@@ -77,9 +91,9 @@ class Example(QWidget):
             #we use a trick child=child to force the current child to be passed.
             childtext = child.name
             btn = QPushButton(childtext, self)
-            btn.setStyleSheet("background-color: #2b2b2b; color: white; height: 100px; width: 100px;")
+            btn.setStyleSheet("background-color: #2b2b2b; color: white; height: 100px; width: 200px; max-width: 200px;")
             if len(child.children) != 0:
-                btn.setStyleSheet("background-color: #4b4b4b; color: white; height: 100px; width: 100px;")
+                btn.setStyleSheet("background-color: #4b4b4b; color: white; height: 100px; max-width: 200px; width: 200px;")
             btn.clicked.connect(lambda state, bound_child=child: self.update_items(bound_child))
             btn.setToolTip('This button represents the ' + childtext + ' block of code')
             if i == 5:
@@ -90,7 +104,7 @@ class Example(QWidget):
 
         if child.parent != root:
             btn = QPushButton("Back", self)
-            btn.setStyleSheet("background-color: #2b2b2b; color: white; height: 100px;")
+            btn.setStyleSheet("background-color: #2b2b2b; color: white; height: 100px; max-width: 200px; width: 200px;")
             btn.clicked.connect(lambda: self.update_items(node.parent))
             grid.addWidget(btn, i+1, j)
         
